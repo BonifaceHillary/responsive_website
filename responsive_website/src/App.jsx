10ea0d4b710c110e6afd6852b33f1ce09df4745e
Boniface Hillary <nkasiboniface@gail.com>
2025-08-15 22:49:17 +0100

    initial commit

--- responsive_website/src/App.jsx
+++ responsive_website/src/App.jsx
@@ -28,7 +28,6 @@ const App = () => {
   const [playState, setPlayState] = useState(false);
 
 
-
   return (
     <div>
       <Navbar />
@@ -36,6 +35,7 @@ const App = () => {
       <Hero />
 
       <div className='container'>
+
         <Title subTitle='Our PROGRAM' title='What We Offer' />
 
       <Programs />
